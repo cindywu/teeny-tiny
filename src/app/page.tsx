a3facc77edc4d541c5d52df52e163c5168e15bb6
Cindy Wu <ciindy.wu@gmail.com>
2023-08-04 19:59:47 -1000

    make home route to /links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-
+import Link from 'next/link'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
+      <Link href="/links">
         <h1 className="text-2xl">teeny tiny</h1>
         <p>production-ready url shortner that *just works*</p>
-      </div>
+      </Link>
     </main>
   )
 }
