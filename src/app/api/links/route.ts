cbb41ae6dba94fccaaf53fd6c7a3d4217dec2fcc
Cindy Wu <ciindy.wu@gmail.com>
2023-08-04 19:43:58 -1000

    added url validation to links form

--- src/app/api/links/route.ts
+++ src/app/api/links/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse, NextRequest } from 'next/server'
+import isValidURL from '@/app/lib/isValidURL'
 
 export async function POST(request: NextRequest) {
   // using standard HTML form
@@ -10,5 +11,11 @@ export async function POST(request: NextRequest) {
     return NextResponse.json({"error": "Invalid request"}, {status: 400})
   }
   const data = await request.json()
+  const url = data && data.url ? data.url : null
+  const validURL = await isValidURL(url, ["teeny-tiny.vercel.app", process.env.NEXT_PUBLIC_VERCEL_URL as string])
+  console.log({validURL})
+  if (!validURL) {
+    return NextResponse.json({"message": `${url} is not valid.`}, {status: 400})
+  }
   return NextResponse.json(data, {status: 201})
 }
---
+++ src/app/lib/isValidURL.ts
@@ -0,0 +1,23 @@
+// copied from gist: https://gist.github.com/codingforentrepreneurs/13e153d28e8c15b7b97331f7813fae9b
+
+// Usage:
+// const isValid = await isValidURL("http://mytest-domain.com", ["mytest-domain.com"])
+// // false
+
+// const isValid = await isValidURL("http://mytest.com", ["mytest-domain.com"])
+// // true
+
+// const isValid = await isValidURL("http://localhost:3000")
+// // false
+export default async function isValidURL(url: string, disallowedDomains: string[]) {
+    // Construct a regular expression pattern to match disallowed domains
+    const disallowedPattern = `^https?:\\/\\/(?:${disallowedDomains.join('|')})\\b`;
+    let disallowedRegex = new RegExp(disallowedPattern, 'i');
+  
+    // Regular expression pattern to match a URL (excluding localhost)
+    const urlPattern = /^(https?:\/\/)?((?!localhost)[\w.-]+)\.([a-z]{2,})(:\d{1,5})?(\/.*)?$/i;
+    let urlRegex = new RegExp(urlPattern);
+  
+    // Test the URL against both URL pattern and disallowed domain pattern
+    return urlRegex.test(url) && !disallowedRegex.test(url);
+  }
