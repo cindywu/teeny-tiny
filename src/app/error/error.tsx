bbcde83a7879fffbd8c2e91579558066ab02ea10
Cindy Wu <ciindy.wu@gmail.com>
2023-08-04 16:40:32 -1000

    error handling for specific route and for generic error

---
+++ src/app/error/error.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { useEffect } from 'react'
+
+export default function Error({ error, reset }: {error: any, reset: any}) {
+    
+  // useEffect is possible here bc of 'use client'
+  useEffect(() => {
+    console.log({error})
+  },[error])
+
+  const retryRequestHandler = () => {
+    reset()
+  }
+  
+  return<div>
+  <h2>Something went wrong on purpose!</h2>
+  <button onClick={retryRequestHandler}>Retry request</button>
+  </div>
+}
---
+++ src/app/error/page.tsx
@@ -0,0 +1,26 @@
+// 'use client'
+
+async function getData() {
+  // 1 endpoint - API ?
+  const endpoint = "http://localhosts:3000/api/posts"
+  const res = await fetch(endpoint) // HTTP GET
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data")
+  }
+  return res.json()
+}
+
+
+export default async function BlogPage(){
+  const data = await getData()
+  const items = data && data.items ? [...data.items] : []
+  console.log({items})
+  return <main>
+    <h1>hello world</h1>
+    <p>posts:</p>
+    {items && items.map((item: {id: number, title: string}, index: number) => {
+      return <li key={`post-${index}`}>{item.title}</li>
+    })}
+    </main>
+}
