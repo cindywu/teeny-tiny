aee76f09f2ec49e673e899e332d803d9ddf16812
Cindy Wu <ciindy.wu@gmail.com>
2023-08-04 19:57:31 -1000

    fix home styles and copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1>teeny-tiny is coming soon!</h1>
+      <div>
+        <h1 className="text-2xl">teeny tiny</h1>
+        <p>production-ready url shortner that *just works*</p>
+      </div>
     </main>
   )
 }
