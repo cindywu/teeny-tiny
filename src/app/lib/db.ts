c52b81d32048b7845093c74e4f807ce7736f0b0b
Cindy Wu <ciindy.wu@gmail.com>
2023-08-05 17:12:16 -1000

    make it work

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -7,5 +7,17 @@ export async function helloWorld() {
   const [dbResponse] = await sql`SELECT NOW();`
   const dbNow = dbResponse && dbResponse.now? dbResponse.now : ""
   const end = new Date()
+
   return {dbNow: dbNow, latency: Math.abs(end.getTime()-start.getTime())}
 }
+
+async function configureDatabase() {
+  const dbResponse = await sql`CREATE TABLE IF NOT EXISTS "links" (
+    "id" serial PRIMARY KEY NOT NULL,
+    "url" text NOT NULL,
+    "created_at" timestamp DEFAULT now()
+  )`
+  console.log("Db response for new table", dbResponse)
+}
+
+configureDatabase().catch(err=>console.log("db config err", err))
