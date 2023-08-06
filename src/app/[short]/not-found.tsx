e241c0d730e41a628daa54936559512ca5eadffd
Cindy Wu <ciindy.wu@gmail.com>
2023-08-05 18:58:27 -1000

    customize 404 not found page

---
+++ src/app/[short]/not-found.tsx
@@ -0,0 +1,7 @@
+// this is not working
+export default function NotFound() {
+  return <>
+    <h1>This was not a valid teeny-tiny.vercel.app link</h1>
+    <p>Status: 404</p>
+  </>
+}
--- src/app/[short]/page.tsx
+++ src/app/[short]/page.tsx
@@ -1,10 +1,16 @@
+import { notFound } from 'next/navigation' 
 import { getShortLinkRecord } from '@/app/lib/db'
+
 export default async function ShortPage({ params }: { params: any }) {
   const { short } = params
+  if (!short) {
+    notFound()
+  }
   const [record] = await getShortLinkRecord(short)
   if (!record) {
-    return <h1>Not Found</h1>
+    notFound() // 404
   }
+  
   return <>
     {JSON.stringify(record)}
   </>
