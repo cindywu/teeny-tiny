936e917364a6c66b1d785c88ad759eb2a08fcf13
Cindy Wu <ciindy.wu@gmail.com>
2023-08-05 16:50:07 -1000

    show db response and latency at /blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -34,10 +34,11 @@ async function getData() {
 export default async function BlogPage(){
   const data = await getData()
   const dbHello = await helloWorld()
-  console.log({dbHello})
+  console.log('dbHello', dbHello)
   const items = data && data.items ? [...data.items] : []
   return <main>
     <h1>hello world</h1>
+    <p>DB Response: {JSON.stringify(dbHello)}</p>
     <p>posts:</p>
     {items && items.map((item: {id: number, title: string}, index: number) => {
       return <BlogCard key={`post-${index}`} title={item.title}/>
